a454f1a6f5a795c0faff8b98193230fa320925fe
PriyaMaurya151085 <priya.maurya@engineering.digital.dwp.gov.uk>
2025-03-06 11:51:20 +0000

    Initial commit
    GitOrigin-RevId: e3eb4b58b28c7753a554d0f67083637e0a44e023

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,18 +5,4 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
-  var rows = document.getElementsByClassName("govuk-table__row");
-  console.log(rows);
-  Array.prototype.forEach.call(
-    rows,
-    (row) => {
-      row.addEventListener("mouseover", (event) => {
-        console.log(event)
-        row.classList.add("grey-row");
-      })
-      row.addEventListener("mouseout", (event) => {
-        row.classList.remove("grey-row");
-      })
-    }
-  )
 })
--- app/routes.js
+++ app/routes.js
@@ -6,99 +6,44 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+// Find an address plugin
+const findAddressPlugin = require("find-an-address-plugin");
+
+findAddressPlugin(router);
 
 // Logging session data  
+// This code shows in the terminal what session data has been saved.
+router.use((req, res, next) => {    
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+   
+  next()  
+})  
 
-router.use((req, res, next) => {
-    const log = {
-        method: req.method,
-        url: req.originalUrl,
-        data: req.session.data
-    }
-    console.log(JSON.stringify(log, null, 2))
+// This code shows in the terminal what page you are on and what the previous page was.
+router.use('/', (req, res, next) => {  
+    res.locals.currentURL = req.originalUrl; //current screen  
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+  
+  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+  
+    next();  
+  });
 
-    next()
-})
+  // Routing for the example journey. 
+  router.post('/country-answer', function(request, response) {
 
-// routes for Outcome reason page
-router.post('/additional-outcomes', function (req, res) {
-    var outcomeReasonVar = req.session.data['outcomeReason']
-    if (outcomeReasonVar == "Full disallowance") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/full-disallowance')
-    } if (outcomeReasonVar == "Partial disallowance") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/partial-disallowance')
-    }
-    if (outcomeReasonVar == "outstandingCAU") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/outstanding-action-outcome')
-    }
-    if (outcomeReasonVar == "no-action-outcome") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/no-action-outcome')
-    }
-})
-
-// routes for Full disallowance reason page
-router.post('/full-disallowance', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/payment-type')
-})
-
-// routes for Full disallowance reason page
-router.post('/partial-disallowance', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/payment-type')
-})
-
-// routes for payment type selection page
-router.post('/payment-type', function (req, res) {
-    var paymentTypeVar = req.session.data['paymentType']
-    if (paymentTypeVar == "Overpayment") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/overpayment-details')
-    } if (paymentTypeVar == "Underpayment") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/underpayment-details')
-    }
-    if (paymentTypeVar == "Underpayments and overpayments") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/overpayment-details')
-    }
-})
-
-///////// routes for overpayment page ////////
-// routes for overpayment amount page selection page
-router.post('/overpayment-details', function (req, res) {
-    var errorTypeVar = req.session.data['errorType']
-    if (errorTypeVar == "Official error") {
-        res.redirect('Vep-ca-mi-alerts/outcomes/total-overpayments-summary')
+    var country = request.session.data['country']
+    if (country == "England"){
+        response.redirect("example/complete")
     } else {
-        res.redirect('Vep-ca-mi-alerts/outcomes/overpayment-amount-recovery')
+        response.redirect("example/ineligible")
     }
 })
 
-// routes for overpayment amount recovery selection page
-router.post('/overpayment-amount-recovery', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/overpayment-civil-penalty')
-})
 
-// routes for overpayment check your answer page
-router.post('/overpayment-civil-penalty', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/total-overpayments-summary')
-})
-
-///////// routes for underpaymemnt page ////////
-
-// routes for underpayment check your answer page
-router.post('/underpayment-details', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/total-underpayments-summary')
-})
-
-
-
-// routes for Outstanding action in CAU reason page
-router.post('/outstanding-action-outcome', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/outcome-reason-summary')
-})
-
-// routes for No action in CAU reason page
-router.post('/no-action-outcome', function (req, res) {
-    res.redirect('Vep-ca-mi-alerts/outcomes/no-action-outcome-summary')
-})
-
-
-
-module.exports = router
+  // Add your routes here
