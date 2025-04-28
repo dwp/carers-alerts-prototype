4911e81fa4a0616af26bd9bdca3fe92eb758bb53
PriyaMaurya151085 <priya.maurya@engineering.digital.dwp.gov.uk>
2025-04-28 20:08:06 +0100

    Removed unwanted files

--- app/routes.js
+++ app/routes.js
@@ -5,45 +5,83 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const fakeCaseData = require('./data/generate-case-data')
 
-// Find an address plugin
-const findAddressPlugin = require("find-an-address-plugin");
 
-findAddressPlugin(router);
+// Add your routes here
 
-// Logging session data  
-// This code shows in the terminal what session data has been saved.
-router.use((req, res, next) => {    
-    const log = {  
-      method: req.method,  
-      url: req.originalUrl,  
-      data: req.session.data  
-    }  
-    console.log(JSON.stringify(log, null, 2))  
-   
-  next()  
-})  
+const express = require('express')
+const dayjs = require('dayjs')
+var objectSupport = require("dayjs/plugin/objectSupport");
+dayjs.extend(objectSupport);
+// viewing session data
 
-// This code shows in the terminal what page you are on and what the previous page was.
-router.use('/', (req, res, next) => {  
-    res.locals.currentURL = req.originalUrl; //current screen  
-    res.locals.prevURL = req.get('Referrer'); // previous screen
-  
-  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
-  
-    next();  
-  });
+// viewing session data
+router.get('*/prototype-admin/view-data', function(req, res){
+  querystring = '';
+  for ( var key in req.session.data )
+    querystring += key +'=' + req.session.data[key] + '&';
+  res.render('prototype-admin/view-data', { data: JSON.stringify( req.session, null, 2), querystring: querystring } )
+});
 
-  // Routing for the example journey. 
-  router.post('/country-answer', function(request, response) {
+//Radio button redirect package
+//const radioButtonRedirect = require('radio-button-redirect')
+//router.use(radioButtonRedirect)
 
-    var country = request.session.data['country']
-    if (country == "England"){
-        response.redirect("example/complete")
-    } else {
-        response.redirect("example/ineligible")
+// Add your routes here - above the module.exports line
+
+//Point to individual routes files for each iteration
+// router.use('/beta/sprint-3', require('./views/beta/sprint-3/routes'));
+
+//EXAMPLE router.use('/beta/sprint-3/prototype-A', require('./views/beta/sprint-3/prototype-A/routes'));
+
+
+
+// router.use('/beta/sprint-:routeVersion', (req, res, next) => {
+//   var routeVersion = req.params.routeVersion
+//   require('./beta/sprint-' + routeVersion + '/routes')(req, res, next)
+// })
+
+// Examples 
+router.post('/test/date-test-answer', function (req, res) {
+
+    const day = Number(req.session.data['TestDay'])
+    const month = Number(req.session.data['TestMonth'])
+    const year = Number(req.session.data['TestYear'])
+
+    const startDate = dayjs({ year : year, month : month-1, day :day})
+
+    const now = dayjs()
+
+    const daysEmployeeOffSick = now.diff(startDate, "day")
+
+    console.log(daysEmployeeOffSick)
+
+    req.session.data['daysEmployeeOffSick'] = daysEmployeeOffSick
+
+    res.redirect('/test/date-results-test')
+
+  })
+
+  router.all('*', (req, res, next) => {
+    if (!req?.session?.data?.caseData) {
+      req.session.data.caseData = fakeCaseData;
     }
-})
+    next();
+  })
+  
+  router.post('/Admin-v4/maternity-tasks', (req, res, next) => {
+    
+    const sortBy = req.body.sort;
+    if (sortBy === 'priority-low') {
+      req.session.data.caseData.sort( (a,b) => a.priority - b.priority );
+      console.log('Sort by priority low')
+    }
+    if (sortBy === 'priority-high') {
+      req.session.data.caseData.sort( (a,b) => b.priority - a.priority );
+      console.log('Sort by priority high')
+    }
+    next();
+  })
 
-
-  // Add your routes here
+module.exports = router
