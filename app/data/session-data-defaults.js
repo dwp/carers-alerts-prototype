fd4c03413b6ad9ce50ac7453e48568ac224e46d2
PriyaMaurya151085 <priya.maurya@engineering.digital.dwp.gov.uk>
2025-05-21 09:44:38 +0100

    latest cosmetic changes
    GitOrigin-RevId: 637003c8bf85ad2b17c1af9262b91eda7cc5da5b

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,7 +1,29 @@
 module.exports = {
 
   // Insert values here
+  "overpayment": [
+    {
+      overpaymentAmount: "£2000",
+      errorType: "Official error"
+    },
+    {
+      overpaymentAmount: "5000",
+      errorType: "Customer error",
+      amountRecoverable: "£1800",
+      amountNotRecoverable: "£400",
+      civilPenaltyApplied: "yes"
+    }
+  ],
+  "underPayment": [
+    {
+      underPaymentAmount: "£2000",
+      errorType: "Official error"
+    },
+    {
+      underPaymentAmount: "5000",
+      errorType: "customer error"
+    }
+  ]
 
-  
 
 }
