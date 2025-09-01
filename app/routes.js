653b728d78ed1d063fccf6c156faaebdff80cf88
Priya.Maurya2 <priya.maurya@engineering.digital.dwp.gov.uk>
2025-09-01 22:13:27 +0100

    content changes fixed
    GitOrigin-RevId: 5e3a482026c6f6aab511b8758fec79609ae5a226

--- app/routes.js
+++ app/routes.js
@@ -243,6 +243,149 @@ router.post('/payment-type-error', function (req, res) {
 router.post('/overpayment-details-error', function (req, res) {
     res.redirect('v2/outcomes/total-overpayments-summary')
 })
+
+// routes for outcome reasons with new content /////////////
+
+router.post('/outcomes_new_content/additional-outcomes', function (req, res) {
+    var outcomeReasonVar = req.session.data['outcomeReason']
+    if (outcomeReasonVar == "Full disallowance") {
+        res.redirect('v2/outcomes-new-content/full-disallowance')
+    } if (outcomeReasonVar == "Partial disallowance") {
+        res.redirect('v2/outcomes-new-content/partial-disallowance')
+    }
+    if (outcomeReasonVar == "outstandingCAU") {
+        res.redirect('v2/outcomes-new-content/outstanding-action-outcome')
+    }
+    if (outcomeReasonVar == "no-action-outcome") {
+        res.redirect('v2/outcomes-new-content/no-action-outcome')
+    }
+    if (outcomeReasonVar == "no-change-to-award") {
+        res.redirect('v2/outcomes-new-content/no-change-to-award-outcome')
+    }
+})
+
+// routes for Full disallowance reason page
+router.post('/full-disallowance', function (req, res) {
+    res.redirect('v2/outcomes-new-content/payment-type')
+})
+
+// routes for Full disallowance reason page
+router.post('/partial-disallowance', function (req, res) {
+    res.redirect('v2/outcomes-new-content/payment-type')
+})
+
+// routes for Full disallowance reason page
+router.post('/task-list-tab', function (req, res) {
+    res.redirect('v2/alerts-profile/task-list-tab')
+})
+// routes for payment type selection page
+router.post('/payment-type', function (req, res) {
+    var paymentTypeVar = req.session.data['paymentType']
+    if (paymentTypeVar == "Overpayment") {
+        res.redirect('v2/outcomes-new-content/overpayment-details')
+    } if (paymentTypeVar == "Underpayment") {
+        res.redirect('v2/outcomes-new-content/underpayment-details')
+    }
+    if (paymentTypeVar == "Overpayment and arrears") {
+        res.redirect('v2/outcomes-new-content/overpayment-details')
+    }
+})
+
+///////// routes for overpayment page ////////
+// routes for overpayment amount page selection page
+router.post('/overpayment-details', function (req, res) {
+    var paymentTypeVar = req.session.data['paymentType']
+    if (paymentTypeVar == "Overpayment and arrears") {
+        res.redirect('v2/outcomes-new-content/underpayment-details-mixed')
+    }
+    res.redirect('v2/outcomes-new-content/total-overpayments-summary')
+
+})
+
+// routes for overpayment amount recovery selection page
+router.post('/overpayment-amount-recovery', function (req, res) {
+    res.redirect('v2/outcomes-new-content/overpayment-civil-penalty')
+})
+
+// routes for overpayment check your answer page
+router.post('/overpayment-civil-penalty', function (req, res) {
+    res.redirect('v2/outcomes-new-content/total-overpayments-summary')
+})
+
+///////// routes for underpaymemnt page ////////
+
+// routes for underpayment check your answer page
+router.post('/underpayment-details', function (req, res) {
+    var paymentTypeVar = req.session.data['paymentType']
+    if (paymentTypeVar == "Overpayment and arrears") {
+        res.redirect('v2/outcomes-new-content/total-payments-summary')
+    }
+    res.redirect('v2/outcomes-new-content/total-underpayments-summary')
+})
+
+// routes for underpayment check your answer page
+router.post('/underpayment-details-mixed', function (req, res) {
+    res.redirect('v2/outcomes-new-content/total-payments-summary')
+})
+
+
+
+// routes for Outstanding action in CAU reason page
+router.post('/outstanding-action-outcome', function (req, res) {
+    res.redirect('v2/outcomes-new-content/outcome-reason-summary')
+})
+
+// routes for No action in CAU reason page
+router.post('/no-action-outcome', function (req, res) {
+    res.redirect('v2/outcomes-new-content/no-action-outcome-summary')
+})
+
+router.post('/no-change-to-award-outcome', function (req, res) {
+    res.redirect('v2/outcomes-new-content/no-change-to-award-summary')
+})
+
+
+// routes for error pages
+
+router.post('/additional-outcomes-error', function (req, res) {
+    var outcomeReasonVar = req.session.data['outcomeReason']
+    if (outcomeReasonVar == "Full disallowance") {
+        res.redirect('v2/error-pages/full-disallowance-error')
+    } if (outcomeReasonVar == "Partial disallowance") {
+        res.redirect('v2/error-pages/partial-disallowance-error')
+    }
+    if (outcomeReasonVar == "outstandingCAU") {
+        res.redirect('v2/error-pages/outstanding-action-outcome-error')
+    }
+    if (outcomeReasonVar == "no-action-outcome") {
+        res.redirect('v2/error-pages/no-action-outcome-error')
+    }
+    if (outcomeReasonVar == "no-change-to-award") {
+        res.redirect('v2/error-pages/no-change-to-award-outcome-error')
+    }
+})
+
+router.post('/additional-outcomes-error', function (req, res) {
+    res.redirect('v2/error-pages/full-disallowance-error')
+})
+
+router.post('/full-disallowance-error', function (req, res) {
+    res.redirect('v2/error-pages/payment-type-error')
+})
+
+router.post('/partial-disallowance-error', function (req, res) {
+    res.redirect('v2/error-pages/payment-type-error')
+})
+
+router.post('/payment-type-error', function (req, res) {
+    res.redirect('v2/error-pages/overpayment-details-error')
+})
+
+
+router.post('/overpayment-details-error', function (req, res) {
+    res.redirect('v2/outcomes-new-content/total-overpayments-summary')
+})
+
 module.exports = router
 
 
